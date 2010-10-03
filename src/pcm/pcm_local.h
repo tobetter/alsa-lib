d75094c4d57903c0f7be5d0096c383c8fe99c8d5
Daniel T Chen <crimsun@ubuntu.com>
2010-10-03 14:56:12 -0400

    This patch is created after applying
    Gbp-Pq: relibtoolise.patch.

--- src/pcm/pcm_local.h
+++ src/pcm/pcm_local.h
@@ -174,16 +174,18 @@ typedef struct {
 } snd_pcm_fast_ops_t;
 
 struct _snd_pcm {
-	void *dl_handle;
+	void *open_func;
 	char *name;
 	snd_pcm_type_t type;
 	snd_pcm_stream_t stream;
 	int mode;
+	long minperiodtime;		/* in us */
 	int poll_fd_count;
 	int poll_fd;
 	unsigned short poll_events;
 	int setup: 1,
-	    monotonic: 1;
+	    monotonic: 1,
+	    compat: 1;
 	snd_pcm_access_t access;	/* access mode */
 	snd_pcm_format_t format;	/* SND_PCM_FORMAT_* */
 	snd_pcm_subformat_t subformat;	/* subformat */
